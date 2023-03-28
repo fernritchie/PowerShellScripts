2699f8f28614272dc2978a9119a099d9d9856870
Fern <55937859+fernritchie@users.noreply.github.com>
2023-03-28 11:29:53 +0100

    Add uninstall software
    Queries GUID, prompt

---
+++ checkSoftwareGuidPromptUninstall.ps1
@@ -0,0 +1,24 @@
+﻿
+#Enter the software name exactly
+$softwareQuery = Read-Host 'What is the software you wish to query?'
+
+#Selects the identifying number and versions number from the name entered
+      $product = Get-WmiObject win32_product | where{$_.name -eq $softwareQuery}
+      
+      Write-Host "Your software version is: $($product.Version)"
+      Write-Host "Your software GUID is:  $($product.IdentifyingNumber)"
+
+#Asks if you would like to uninstall the software, uses the /qn and /norestart switch, change these where necessary
+
+      $uninstallSoftware = Read-Host 'Would you like to uninstall this software? (Y or N)'
+
+      if ( $uninstallSoftware -eq "Y" ) {
+
+      Start-Process "C:\Windows\System32\msiexec.exe" `
+      -ArgumentList "/x $($product.IdentifyingNumber) /qn /norestart" -Wait
+
+      }
+
+      else {
+      Write-Host 'Have a nice day!'
+      }
---
+++ uninstallSoftwareRemote.ps1
@@ -0,0 +1,7 @@
+﻿
+#Where the script says ENTER SOFTWARE HERE, enter the software you wish to uninstall. 
+#Currently the -ArgumentList shows /qn (quiet uninstall) /norestart. Add or remove switches where necessary.
+      $product = Get-WmiObject win32_product | where{$_.name -eq "ESET Endpoint Security"}
+      $product.IdentifyingNumber
+      Start-Process "C:\Windows\System32\msiexec.exe" `
+      -ArgumentList "/x $($product.IdentifyingNumber) /qn /norestart" -Wait
